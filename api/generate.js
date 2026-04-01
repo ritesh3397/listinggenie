d62c8c1d64c3b60ca0fe9259f8293f3d0cc4d4e0
ritesh3397 <ritesh.12mzp@gmail.com>
2026-04-01 05:33:40 +0530

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -44,7 +44,7 @@ export default async function handler(req, res) {
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
-        model: "mixtral-8x7b-32768",
+        model: "llama-3.3-70b-versatile"
         messages: [
           {
             role: "system",
